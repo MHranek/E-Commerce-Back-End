1b08e69628c00b57132e24f6b055be59e4b52ca7
Michael Hranek <mhranek@comcast.net>
2022-01-22 20:10:45 -0500

    Added basic GET routes for Categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,26 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    //TODO be sure to include its associated Products
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    //TODO be sure to include its associated Products
+    const categoryData = await Category.findOne(req.params.id);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
