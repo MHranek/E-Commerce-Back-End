e36c8400a0b189102ffa7d565e80b990ea9eebfe
Michael Hranek <mhranek@comcast.net>
2022-01-23 13:49:55 -0500

    Added routes for categories, DELETE category not cascading

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,22 +19,60 @@ router.get('/:id', async (req, res) => {
   try {
     // be sure to include its associated Products
     const categoryData = await Category.findByPk(req.params.id, { include: Product });
+    if (!categoryData) {
+      res.status(404).json("No category with that ID exists");
+      return;
+    }
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updateCategory) {
+      res.status(404).json("No category with that ID exists");
+      return;
+    }
+    res.status(200).json("Successfully updated category");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteCategory) {
+      res.status(404).json("No category with that ID exists");
+      return;
+    }
+    res.status(200).json("Successfully deleted category");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
   // create a new tag
   try {
     await Tag.create(req.body);
-    res.status(200).json("Successfully created tag");
+    res.status(201).json("Successfully created tag");
   } catch (err) {
     res.status(500).json(err);
   }
@@ -42,7 +42,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const updateTag = Tag.update(req.body, {
+    const updateTag = await Tag.update(req.body, {
       where: {
         id: req.params.id
       }
@@ -60,6 +60,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
+    await ProductTag.destroy({ where: { tag_id: req.params.id } });
     const deleteTag = await Tag.destroy({
       where: {
         id: req.params.id
